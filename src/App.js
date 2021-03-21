a909b82bb2af8fbe97bdfb743f9a8802b538b998
salih4131 <ub4131@gmail.com>
2021-03-21 15:08:39 +0100

    the first React programming has done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h3>Input Format</h3>
+    <p>first line contains <i><b>N.N</b></i> lines follow, each having a PAN number. </p>
+    <h3>Constraints</h3>
+    <ul>
+      <li> 1 ≤ N ≤ 10 </li>
+      <li>Each char is an uppercase letter,i.e.,<i><b>char</b></i> ∈ [<i><b>'A','Z'</b></i>].</li>
+      <li>Each digit lies between 0 and 9, i.e.,<i><b>digit</b></i> ∈ [<b>0,9</b>]. </li>
+      <li>The lenght of the PAN number is always 10, i.e.,<i><b>lenght(PAN)</b></i><b>=10</b></li>
+      <li>Every character in PAN is either char or digit satisfying the above constraints.</li>
+    </ul>
+    <h3>Output Format</h3>
+    <p>For every PAN number listed, print YES if it is valid and NO if it isnt't.</p>
+    
+  </div>
+    
   );
 }
 
